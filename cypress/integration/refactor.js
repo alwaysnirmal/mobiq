a1eddf4f92bffe2cbabbd1e78dd31bb07589597c
Nirmal Hasmukh Dholakhia <>
2020-07-18 23:01:22 +0200

    added negative tests

--- cypress/integration/refactor.js
+++ cypress/integration/refactor.js
@@ -2,19 +2,22 @@
 
 context('API Tests for Checking Format of Email in comments of Posts by Particular UserID', () => {
 
-    it('Get user details', () => {
+    it('Get User details', () => {
         //requesting user info of all avaiable users
         cy.request('/users')
         .then((response) => {
             expect(response.status).to.eq(200);
             expect(response).to.have.property('headers')
-            expect(response).to.have.property('duration')
+            expect(response).to.have.property('duration').and.be.a('number')
             expect(response.body[0],'Userid is of Type Number').property('id').to.be.a('number');
-            expect(response.body,'Lenght is correct').to.have.length(10);
+            expect(response.headers).to.have.property('content-type').to.contain('application/json; charset=utf-8');
+            expect(response.body,'Number of Users are 10').to.have.length(10);
+           
+            //Storing Data in User file to be used afterwards
             cy.writeFile('cypress/fixtures/userDetails.json',response.body);
-            //retriving the object related to partocular user name and then finding the userid of particular user
+            
         })
-        //function to search Username
+        //function to search Username and retreive UserID
         cy.findUser('Delphine').then((userID) => {
                  cy.wrap(userID).as('userId')
         });
@@ -32,6 +35,7 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
             expect(response.status).to.eq(200);
             expect(response).to.have.property('headers')
             expect(response).to.have.property('duration')
+            //Asserting that Number of Posts by particular user is 10
             expect(response.body).to.have.length(10);
             //storing the response in fixture file to be used in other parts of tests
             cy.writeFile('cypress/fixtures/posts.json',response.body);
@@ -47,12 +51,12 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
             for (var i of posts){    
                 cy.request('/comments?postId='+i.id)
                 .then((commentResponse) => {
-                    expect(commentResponse.body).to.have.length(5);
+                    expect(commentResponse.body,'Comments count per post is 5').to.have.length(5);
                     var commentsCount = Object.keys(commentResponse.body).length;
                     for(var x=0;x<commentsCount;x++){
                         cy.validateEmail(commentResponse.body[x].email)
                         .then((emailValidationResponse) => {
-                            expect(emailValidationResponse).to.be.true;
+                            expect(emailValidationResponse,'Email is of Correct Format').to.be.true;
                         });                          
                     }           
                 });
@@ -61,16 +65,23 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
     });
     
     it('Invalid Post ID',() => {
-        cy.request('https://jsonplaceholder.typicode.com/comments?postId=101')
+        //PostID 101 does not exist , Empty array should be retrieved 
+        cy.request('/comments?postId=101')
         .then((response) => {
-            console.log(response)
-            expect(response.status).to.eq(200);
             expect(response).to.have.property('headers')
             expect(response).to.have.property('duration')
-            expect(response.body).to.have.length(0);
+            expect(response.body,'Empty Array is returned').to.have.length(0);
 
         })
 
     })
+
+    it('InValid User Id while retrieving the Posts based on UserID' ,() => {
+        cy.request('/posts?userId=1001')
+        .then((response) => {
+            expect(response.body,'Empty Array is returned').to.have.length(0);
+        })
+    })
+
 });
 
