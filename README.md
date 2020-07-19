2c17efbea94f0c50c06b5554f086cd49b3070f0f
Nirmal Hasmukh Dholakhia <>
2020-07-19 15:29:51 +0200

    Name change and some structuarl changes of the tests

---
+++ README.md
@@ -0,0 +1,31 @@
+## Assignment By Nirmal Hasmukh Dholakhia 
+
+Tool used
+Cypress
+
+Library used
+chai assertion Library
+
+## Installation
+
+```bash
+npm install
+```
+## How to Run
+
+```bash
+./node_modules/.bin/cypress run
+```
+or
+```bash
+npx cupress run
+```
+## Where to find test 
+
+Spec Files - cypress/integration/
+Support Files - cypress/support/
+
+## Reprot
+
+mochawesome-report/mochaawesome.html
+In addiiton to this , There is always a UI Report being displayed during the test run in non-headless mode
--- cypress/integration/comments.js
+++ cypress/integration/comments.js
@@ -8,7 +8,6 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
         .then((response) => {
             console.log(response);
             expect(response.status).to.eq(200);
-            expect(response).to.have.property('headers')
             expect(response).to.have.property('duration').and.be.a('number');
             expect(response.headers).to.have.property('content-type').to.contain('application/json; charset=utf-8');
             expect(response.body,'Number of Users are 10').to.have.length(10);
@@ -55,8 +54,16 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
                     expect(commentResponse.body,'Comments count per post is 5').to.have.length(5);
                     var commentsCount = Object.keys(commentResponse.body).length;
                     for(var x=0;x<commentsCount;x++){
+                        expect(commentResponse.body[x]).to.have.property('postId').and.is.a('number');
+                        expect(commentResponse.body[x]).to.have.property('id').and.is.a('number');
+                        expect(commentResponse.body[x]).to.have.property('name').and.is.a('string');
+                        expect(commentResponse.body[x]).to.have.property('email').and.is.a('string');
+                        expect(commentResponse.body[x]).to.have.property('body').and.is.a('string');
+
+                        let email = commentResponse.body[x].email
+
                         //Validating Email Format 
-                        cy.validateEmail(commentResponse.body[x].email)
+                        cy.validateEmail(email)
                         .then((emailValidationResponse) => {
                             expect(emailValidationResponse,'Email is of Correct Format').to.be.true;
                         });                          
@@ -65,8 +72,10 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
             }
         }); 
     });
-    
-    it('Invalid Post ID',() => {
+});
+
+describe("Negative Test Scenario's / Inavlaid query parameters",() => {
+    it('Validate `response when Invalid Post ID is provided',() => {
         //PostID 101 does not exist , Empty array should be retrieved 
         cy.request('/comments?postId=101')
         .then((response) => {
@@ -76,11 +85,12 @@ context('API Tests for Checking Format of Email in comments of Posts by Particul
         });
     });
 
-    it('InValid User Id while retrieving the Posts based on UserID' ,() => {
+    it('Validate Response When Invalid User Id is provided retrieving the Posts based on UserID' ,() => {
+        //User ID 1001 does not exist - Empty array should be returned
         cy.request('/posts?userId=1001')
         .then((response) => {
             expect(response.body,'Empty Array is returned').to.have.length(0);
         });
     });
-});
+})
 
